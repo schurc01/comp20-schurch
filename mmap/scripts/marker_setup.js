8b8f73ad362ffcfa486a2e19dfd9b6d30e6bfa71
schurc01 <sjchurch18@gmail.com>
2014-10-19 03:15:43 -0400

    distance function works

--- mmap/scripts/marker_setup.js
+++ mmap/scripts/marker_setup.js
@@ -11,38 +11,39 @@ var my_infowindow = new google.maps.InfoWindow({
 var infowindow = new google.maps.InfoWindow();
 var request = new XMLHttpRequest();
 var mousepos = new google.maps.LatLng();
+var distance;
 
 //creates an initial map with no markers
 function initialize() {
 	myOptions = {
-    zoom: 14, 
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  getMyLocation();
+    	zoom: 14, 
+    	center: latlng,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP
+  	};
+  	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  	getMyLocation();
 };
 
 //find my location using geolocation
 function getMyLocation() {
  	if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      my_lat = position.coords.latitude;
-	    my_long = position.coords.longitude;
-	    renderMeOnMap();
-    });
-  }
-  //you're using IE
-  else {
-    alert("Geolocation is not supported by your web browser!");
-  }
+    	navigator.geolocation.getCurrentPosition(function(position) {
+    		my_lat = position.coords.latitude;
+	    	my_long = position.coords.longitude;
+	    	renderMeOnMap();
+    	});
+  	}
+  	//you're using IE
+  	else {
+   		alert("Geolocation is not supported by your web browser!");
+  	}
 };
 
 //renders map with geolocation marker of my location
 function renderMeOnMap() {
 	latlng = new google.maps.LatLng(my_lat, my_long);
 	//my kitten marker image
-  var image = "http://placekitten.com/g/40/40";
+  	var image = "http://placekitten.com/g/40/40";
 
 	me_marker = new google.maps.Marker({
 		position: latlng,
@@ -53,7 +54,7 @@ function renderMeOnMap() {
 
 	//open info window on click of marker
 	google.maps.event.addListener(me_marker, 'click', function() {
-    my_infowindow.close();
+    	my_infowindow.close();
 		my_infowindow.setContent(me_marker.title);
 		my_infowindow.open(map, me_marker);
 	});
@@ -68,51 +69,51 @@ function renderMeOnMap() {
 //requests data from datastore
 function getData() {
 	try {
-    //open request
-	request.open('POST', post_url, true);
-    //set request header
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    //send the request with my geolocation information
-	  request.send("login=Horace&lat=" + my_lat + "&lng=" + my_long);
+    	//open request
+		request.open('POST', post_url, true);
+    	//set request header
+    	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    	//send the request with my geolocation information
+	  	request.send("login=Horace&lat=" + my_lat + "&lng=" + my_long);
 
-    //function that executes on a readyState change
+    	//function that executes on a readyState change
 		request.onreadystatechange = function() {
-      //checks whether the request was successful
-		  if (request.readyState == 4 && request.status == 200) {
-        //parses data
-			  var parsed_data = JSON.parse(request.responseText);
-        //creates markers for students
-			  for (var i = 0; i < parsed_data['students'].length; i++)
-				  createStudentMarker(parsed_data['students'][i]);
-        //creates markers for characters
-			  for (var i = 0; i < parsed_data['characters'].length; i++)
-				  createCharacterMarker(parsed_data['characters'][i]);
-	    }
-    };
-  }
-  //if the request fails
-  catch(failed) {
-	  request = null;
-      alert("Could not retrieve data from datastore!");
-  }
+      		//checks whether the request was successful
+		  	if (request.readyState == 4 && request.status == 200) {
+        		//parses data
+			  	var parsed_data = JSON.parse(request.responseText);
+        		//creates markers for students
+			  	for (var i = 0; i < parsed_data['students'].length; i++)
+				  	createStudentMarker(parsed_data['students'][i]);
+        		//creates markers for characters
+			 	for (var i = 0; i < parsed_data['characters'].length; i++)
+				 	createCharacterMarker(parsed_data['characters'][i]);
+	    	}
+    	};
+  	}
+  	//if the request fails
+  	catch(failed) {
+		request = null;
+      	alert("Could not retrieve data from datastore!");
+  	}
 };
 
 //creates a marker for each student on screen
 function createStudentMarker(student) {
 	var student_latlng = new google.maps.LatLng(student.lat, student.lng);
-  var contentString = "login: " + student.login + "<br>lat/lng: " + student.lat +
+  	var contentString = "login: " + student.login + "<br>lat/lng: " + student.lat +
                       ", " + student.lng + "<br>timestamp: " + student.created_at;
 
-  //creats a marker for them with location
+  	//creats a marker for them with location
 	var marker = new google.maps.Marker({
 		map: map,
 		position: student_latlng
 	});
 
-  //shows information in infowindow on a click
+  	//shows information in infowindow on a click
 	google.maps.event.addListener(marker, 'click', function() {
-	  infowindow.close();
-	  infowindow.open(map, this);
+	  	infowindow.close();
+	  	infowindow.open(map, this);
 	});
 };
 
@@ -120,17 +121,16 @@ function createStudentMarker(student) {
 function createCharacterMarker(character) {
 	var character_latlng = new google.maps.LatLng(character.loc.latitude,
                                                 character.loc.longitude);
-  //finds image for the character
+  	//finds image for the character
 	var image = "images/" + character.name + ".png";
-  var contentString = "name: " + character.name + "<br>lat/lng: " 
+ 	var contentString = "name: " + character.name + "<br>lat/lng: " 
                       + character.loc.latitude + ", " + character.loc.longitude
                       + "<br>note: " + character.loc.note;
-	var distance;
 
-  //calculates distance between me and character
+  	//calculates distance between me and character
 	distanceBetweenMeAndCharacter(character.loc);
 
-  //creates marker for character with image
+  	//creates marker for character with image
 	var marker = new google.maps.Marker({
 		map: map,
 		position: character_latlng,
@@ -139,43 +139,43 @@ function createCharacterMarker(character) {
 
 	//shows info window on click
 	google.maps.event.addListener(marker, 'click', function() {
-    infowindow.close();
-    infowindow.setContent(contentString);
-    infowindow.open(map, this);
+    	infowindow.close();
+    	infowindow.setContent(contentString);
+    	infowindow.open(map, this);
 	});
 
   //creates polyline between me and character
 	var polyline = new google.maps.Polyline({
-	  path: [character_latlng, latlng],
-	  map: map,
-    geodesic: false,
-	  strokeColor: '#377FD3',
-	  strokeOpacity: 0.7,
-	  strokeWeight: 3
-  });
+		path: [character_latlng, latlng],
+	  	map: map,
+    	geodesic: false,
+	  	strokeColor: '#377FD3',
+	  	strokeOpacity: 0.7,
+	  	strokeWeight: 3
+  	});
 
-	var rectangle = new google.maps.Rectangle({
-    strokeColor: '#0B1C48',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#377FD3',
-    fillOpacity: 0.35,
-    map: map,
-};
-
+	// var rectangle = new google.maps.Rectangle({
+ //    	strokeColor: '#0B1C48',
+ //    	strokeOpacity: 0.8,
+ //    	strokeWeight: 2,
+ //    	fillColor: '#377FD3',
+ //    	fillOpacity: 0.35,
+ //    	map: map
+	// });
+}
 //calculates distance between me and the character
 function distanceBetweenMeAndCharacter(character) {
-  //radius of the earth in KM
+  	//radius of the earth in KM
 	var earthRadius = 6371;
-  //character latitude in radians
+  	//character latitude in radians
 	var cLatInRadians = character.latitude.toRadians();
-  //character longitude in radians
-  var cLongInRadians = character.longitude.toRadians();
+  	//character longitude in radians
+  	var cLongInRadians = character.longitude.toRadians();
 
 	var changeInLat = (character.latitude - my_lat).toRadians();
 	var changeInLong = (character.longitude - my_long).toRadians();
 
-  //calculations for the Haversine formula
+  	//calculations for the Haversine formula
 	var a = Math.sin(changeInLat/2) * Math.sin(changeInLat/2) +
 			Math.cos(cLatInRadians) * Math.cos(cLongInRadians) *
 			Math.sin(changeInLong/2) * Math.sin(changeInLong/2);
@@ -186,5 +186,5 @@ function distanceBetweenMeAndCharacter(character) {
 
 //turns number to radians
 Number.prototype.toRadians = function() {
-  return this * Math.PI / 180;
+  	return this * Math.PI / 180;
 }
